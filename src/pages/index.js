7e485c535886262ceafb39267d95cad32578b4e7
jounaidyoussef <jounaid.ysf@gmail.com>
2022-02-21 17:40:15 +0100

    add portfolio pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -133,7 +133,7 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         Congratulations
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        <span style={headingAccentStyles}>— you just made a New site! </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
