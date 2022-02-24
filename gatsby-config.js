83abaf1be337b41b34ed8440900104764cc4e652
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-02-24 17:27:55 +0100

    configure and add svg sociam media icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,14 @@ module.exports = {
         ],
         display: 'swap'
       }
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          exclude: /\.back\.svg$/,
+        }
+      }
     }
   ]
 };
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,46 +1,50 @@
 import React from "react"
 import { Link } from "gatsby"
 import "./index.scss"
+import Github from "../../assets/github.svg"
+import Linkedin from "../../assets/linkedin.svg"
+import Twitter from "../../assets/twitter.svg"
+import Instagram from "../../assets/instagram.svg"
+import Dribbble from "../../assets/dribbble.svg"
 
-const Footer = () => (
+const Footer = () => {
+  return(
   <footer className="container-footer">
-    <div className="footer-container">
       {/* <Link to="/">
         <Logo className="logo" alt="Logo" />
       </Link> */}
-      <div className="menu">
-      <Link
-           to="/blog"
-           // activeClassName="active"
-           // className="item"
-           aria-label="View Blog page"
-           >Blog</Link>
-                   <Link
-           to="/oss"
-           // activeClassName="active"
-           // className="item"
-           aria-label="View OSS page"
-           >OSS</Link>
-                   <Link
-           to="/projects"
-           // activeClassName="active"
-           // className="item"
-           aria-label="View Projects page"
-           >Projects</Link>
-                   <Link
-           to="/about"
-           // activeClassName="active"
-           // className="item"
-           aria-label="View About page"
-           >About</Link>
+      <div className="links">
+        <p>Got a project? Let’s talk</p>
+      <div className="contact">
+      <a className="getintouch" href="#">Leave a Message</a>
       </div>
-    </div>
-    <div><p>H</p></div>
-    <div><p>H</p></div>
+      <div className="social-icons">
+      <a
+      href="https://www.linkedin.com/in/jounaidyoussef/">
+      <Linkedin />
+    </a>
+    <a
+      href="https://github.com/JounaidYoussef">
+      <Github />
+    </a>
+    <a
+      href="https://podcasts.apple.com/us/podcast/geeksblabla/id1449493227">
+      <Twitter />
+    </a>
+    <a
+      href="https://podcasts.apple.com/us/podcast/geeksblabla/id1449493227">
+      <Instagram />
+    </a>
+    <a
+      href="https://podcasts.apple.com/us/podcast/geeksblabla/id1449493227">
+      <Dribbble />
+    </a>
+      </div>
+      </div>
 
-    <div><p>H</p></div>
 
   </footer>
-)
+  )
+}
 
 export default Footer
