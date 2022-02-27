e5e4bd6aea8d3b6d72560c9b55be065f5775cb72
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-02-27 23:28:05 +0100

    add and configure Eslint and Prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,56 @@
 module.exports = {
   siteMetadata: {
-      title: `Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'Portfolio',
+    siteUrl: 'https://www.yourdomain.tld'
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-image', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./public/static/images"
+      name: 'images',
+      path: './public/static/images'
     },
-    __key: "images"
+    __key: 'images'
   },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Poppins\:100,200,300,400,500,600,700,800,900`,
-          `Oxygen\:300,400,700`,
-        ],
-        display: 'swap'
-      }
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          exclude: /\.back\.svg$/,
+  {
+    resolve: 'gatsby-plugin-prettier-eslint',
+    options: {
+      prettier: {
+        patterns: [
+          // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+          '**/*.{css,scss,less}',
+          '**/*.{json,json5}',
+          '**/*.{graphql}',
+          '**/*.{md,mdx}',
+          '**/*.{html}',
+          '**/*.{yaml,yml}'
+        ]
+      },
+      eslint: {
+        patterns: '**/*.{js,jsx,ts,tsx}',
+        customOptions: {
+          fix: true,
+          cache: true
         }
       }
     }
+  },
+  {
+    resolve: 'gatsby-plugin-google-fonts',
+    options: {
+      fonts: [
+        'Poppins:100,200,300,400,500,600,700,800,900',
+        'Oxygen:300,400,700'
+      ],
+      display: 'swap'
+    }
+  },
+  {
+    resolve: 'gatsby-plugin-react-svg',
+    options: {
+      rule: {
+        exclude: /\.back\.svg$/
+      }
+    }
+  }
   ]
-};
+}
