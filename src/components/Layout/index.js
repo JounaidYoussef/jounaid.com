0891f8029a58441d61ee67b36db2bd196568e815
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-02-28 16:46:21 +0100

    updated layout props and add contact page

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -12,12 +12,14 @@ const container = {
   padding: '0 0 0 0',
 };
 
-export default ({ children }) => (
-  <div style={container}>
-    <div style={styles}>
-      <Header />
-      {children}
+export default ({ children, isFooterDisabled = false }) => {
+  return (
+    <div style={container}>
+      <div style={styles}>
+        <Header />
+        {children}
+      </div>
+      {isFooterDisabled ? null : <Footer />}
     </div>
-    <Footer />
-  </div>
-);
+  );
+};
