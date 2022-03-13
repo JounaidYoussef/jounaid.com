ca98d5d529d905731deaccce284a18f69b535592
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-03-13 03:04:33 +0100

    add BlogPostCard boilerplate

--- src/components/Cards/BlogPostCard.js
+++ src/components/Cards/BlogPostCard.js
@@ -1,11 +1,83 @@
 import React from "react"
 import "./index.scss"
+import { StaticImage } from "gatsby-plugin-image"
 
 const BlogPostCard = () => {
+  const mainpost = false
   return (
-    <div>
-      <p> BlogPostCard </p>
-    </div>
+    <>
+      {mainpost ? (
+        <div className="mainPostContainer">
+          <div className="mainPostPicture">
+            <StaticImage
+              src="../../assets/bb.jpg"
+              alt="Personal Picture"
+              placeholder="blurred"
+            />
+          </div>
+          <div>
+            <p className="mainPostDate">September 22, 2018</p>
+            <p className="mainPostTitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </p>
+            <p className="mainPostDescription">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              pharetra, augue eget volutpat fringilla, nisi sem tincidunt odio,
+              eget bibendum libero felis eget purus. Pellentesque condimentum
+              semper lacus, nec efficitur lectus rhoncus eu. Donec ac accumsan
+              purus. Vestibulum aliquam metus at augue tincidunt, non luctus
+              nunc consectetur.
+            </p>
+            <div>
+              <a href="./blog" className="tag">
+                Dev
+              </a>
+              <a href="./blog" className="tag">
+                Git
+              </a>
+              <a href="./blog" className="tag">
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="secPostContainer">
+          <div className="secPostPicture">
+            <StaticImage
+              src="../../assets/bb.jpg"
+              alt="Personal Picture"
+              placeholder="blurred"
+            />
+          </div>
+          <div>
+            <p className="secPostDate">September 22, 2018</p>
+            <p className="secPostTitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </p>
+            <p className="secPostDescription">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              pharetra, augue eget volutpat fringilla, nisi sem tincidunt odio,
+              eget bibendum libero felis eget purus. Pellentesque condimentum
+              semper lacus, nec efficitur lectus rhoncus eu. Donec ac accumsan
+              purus. Vestibulum aliquam metus at augue tincidunt, non luctus
+              nunc consectetur.
+            </p>
+            <div>
+              <a href="./blog" className="tag">
+                Dev
+              </a>
+              <a href="./blog" className="tag">
+                Git
+              </a>
+              <a href="./blog" className="tag">
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
