af24b3d7afd978fe92906ead74e730890946f95d
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-03-13 18:05:28 +0100

    added repoCard component elements

--- src/components/Cards/RepoCard.js
+++ src/components/Cards/RepoCard.js
@@ -1,10 +1,33 @@
 import React from "react"
 import "./index.scss"
+import Fork from "../../assets/fork.svg"
+import Star from "../../assets/star.svg"
 
 const RepoCard = () => {
   return (
-    <div>
-      <p> RepoCard </p>
+    <div className="repoContainer">
+      <h3 className="repoTitle">Jounaid.com</h3>
+      <p className="repoDesc">
+        This is a personal Portfolio where I share and display awesome topics
+        and projects that may interest you.
+      </p>
+      <div className="repoFooter">
+        <div className="repoTags">
+          <p className="repo-tag">tags</p>
+          <p className="repo-tag">tags</p>
+          <p className="repo-tag">tags</p>
+        </div>
+        <div id="repoStats">
+          <div className="inline-repo-stats">
+            <p>4</p>
+            <Fork />
+          </div>
+          <div className="inline-repo-stats">
+            <p>5</p>
+            <Star />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
