e04050a2ef0950a1f43c89ec292878dbe19018b6
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-03-15 23:56:11 +0100

    add Contact form aligned Image

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -18,48 +18,58 @@ const ContactForm = () => {
             directly or fill out the form and I will get back to you promptly.
           </p>
         </div>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          className="contact-form"
-          data-netlify-honeypot="bot-field"
-          action="/thanks"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <input type="hidden" name="bot-field" />
-          <div className="contact-item">
-            <label id="contact-label">Name</label>
-            <input
-              placeholder="Enter your name"
-              type="text"
-              name="name"
-              required
+        <div className="form-container">
+          <div className="contact-image">
+            <StaticImage
+              src="../../assets/1.jpg"
+              alt="Personal Picture"
+              width={400}
+              height={200}
             />
           </div>
-          <div className="contact-item">
-            <label id="contact-label">Email Adress</label>
-            <input
-              placeholder="Enter your email address"
-              type="email"
-              name="email"
-              required
-            />
-          </div>
-          <div className="contact-item-long">
-            <label id="contact-label">Your Message</label>
-            <textarea
-              placeholder="Hi, I think we need a design system for our products at X. How soon can you hop on to discuss this"
-              rows={7}
-              name="message"
-              required
-            ></textarea>
-          </div>
-          <div className="actions">
-            {/* type="submit" */}
-            <a className="send">Send</a>
-          </div>
-        </form>
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            className="contact-form"
+            data-netlify-honeypot="bot-field"
+            action="/thanks"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="bot-field" />
+            <div className="contact-item">
+              <label id="contact-label">Name</label>
+              <input
+                placeholder="Enter your name"
+                type="text"
+                name="name"
+                required
+              />
+            </div>
+            <div className="contact-item">
+              <label id="contact-label">Email Adress</label>
+              <input
+                placeholder="Enter your email address"
+                type="email"
+                name="email"
+                required
+              />
+            </div>
+            <div className="contact-item-long">
+              <label id="contact-label">Your Message</label>
+              <textarea
+                placeholder="Hi, I think we need a design system for our products at X. How soon can you hop on to discuss this"
+                rows={7}
+                name="message"
+                required
+              ></textarea>
+            </div>
+            <div className="actions">
+              {/* type="submit" */}
+              <a className="send">Send</a>
+            </div>
+          </form>
+        </div>
       </div>
       <div className="social">
         <SocialBar fill="#4C596A" />
