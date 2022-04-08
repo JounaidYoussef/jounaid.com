b86568692f9fd97025217287790603dadfa50240
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-04-08 13:40:49 +0200

    add blogpost card list layout

--- src/components/Cards/BlogPostCard.js
+++ src/components/Cards/BlogPostCard.js
@@ -6,14 +6,13 @@ import { GatsbyImage } from "gatsby-plugin-image"
 const BlogPostCard = ({ post }) => {
   return (
     <Link to={post.fields.slug} aria-label={`View ${post.frontmatter.title}`}>
-      <div>
-        <div>
-          <GatsbyImage
-            image={post.frontmatter.banner.childImageSharp.gatsbyImageData}
-            alt="hello"
-          />
-        </div>
-        <div>
+      <div className="list-blogpost-container">
+        <GatsbyImage
+          image={post.frontmatter.banner.childImageSharp.gatsbyImageData}
+          alt="hello"
+          className="list-blogpost-image-container"
+        />
+        <div className="list-blogpost-info-container">
           <h2>{post.frontmatter.title}</h2>
           <p>{post.excerpt}</p>
           <span />
