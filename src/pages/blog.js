6a9b37f812519a714e43b8728b31a41ca5b954d8
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-04-08 15:44:07 +0200

    add grid list switch buttons

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,16 +6,54 @@ import { GatsbyImage } from "gatsby-plugin-image"
 // markup
 const Blog = ({ data }) => {
   const { posts } = data.blog
-
+  const [toggleViewMode, setToggleViewMode] = React.useState(true)
   return (
     <Layout>
       <main>
         <title>Blog Page</title>
         <div>
-          <h1>My blog posts</h1>
+          <h2>title</h2>
+          <div
+          // style={{
+          //   display: 'flex',
+          //   flexDirection: 'row',
+          //   alignItems: 'center',
+          // }}
+          >
+            <button
+              style={{
+                border: 0,
+                padding: "0.4em",
+                borderRadius: "5px",
+                backgroundColor: toggleViewMode ? "#e1e1e1" : "transparent",
+              }}
+              onClick={() => setToggleViewMode(true)}
+            >
+              <p>grid</p>
+            </button>
+            <button
+              style={{
+                marginLeft: "0.4em",
+                border: 0,
+                padding: "0.4em",
+                borderRadius: "5px",
+                backgroundColor: !toggleViewMode ? "#e1e1e1" : "transparent",
+              }}
+              onClick={() => setToggleViewMode(false)}
+            >
+              <p>list</p>
+            </button>
+          </div>
+        </div>
+        <div className={toggleViewMode ? "grid-layout" : "list-layout"}>
           {posts.map((post) => (
             <article key={post.id}>
-              <BlogPostCard post={post} />
+              {console.log(post.id)}
+              <BlogPostCard
+                start={post.key == 1}
+                viewMode={toggleViewMode}
+                post={post}
+              />
             </article>
           ))}
         </div>
