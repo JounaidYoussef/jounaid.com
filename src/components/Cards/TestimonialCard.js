97a425b38635d4cab02e4a0bbbc760d0e5bd6ce6
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-04-26 19:13:18 +0200

    add testimonialCard content

--- src/components/Cards/TestimonialCard.js
+++ src/components/Cards/TestimonialCard.js
@@ -1,10 +1,32 @@
 import "./index.scss"
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import Quote from "../../assets/quote.svg"
 
 const TestimonialCard = () => {
   return (
-    <div>
-      <p> TestimonialCard </p>
+    <div className="testimonialCard-container">
+      <div className="col">
+        <Quote />
+        <text style={{ fontSize: 14, fontWeight: "400" }}>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
+          fermentum orci. Suspendisse consequat tincidunt eleifend. Sed ac
+          lectus facilisis felis venenatis efficitur vel feugiat turpis. Donec
+          eget dui sit amet lectus vulputate viverra. Mauris sem quam, fringilla
+          quis semper et, tincidunt sit amet erat. Mauris gravida mauris at
+          lectus hendrerit blandit. Ut eu ornare risus.
+        </text>
+      </div>
+      <div style={{ width: "40%" }}>
+        <StaticImage
+          src="../../assets/aa.jpg"
+          alt="Personal Picture"
+          placeholder="blurred"
+          width={160}
+          height={160}
+        />
+      </div>
     </div>
   )
 }
--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,11 +1,12 @@
 import "./index.scss"
 import React from "react"
+import TestimonialCard from "../Cards/TestimonialCard"
 
 export default () => {
   return (
     <div className="testimonials-container">
       <div>
-        <p>testimonials</p>
+        <TestimonialCard />
       </div>
     </div>
   )
