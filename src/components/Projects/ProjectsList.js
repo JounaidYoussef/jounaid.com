fa4f706808ead71f4438aa85b113bc4303203cdb
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-04-30 15:51:48 +0200

    add ProjectsList Component

---
+++ src/components/Projects/ProjectsList.js
@@ -0,0 +1,17 @@
+import "./index.scss"
+import React from "react"
+import BlogPostCard from "../Cards/BlogPostCard"
+import ProjectCard from "../Cards/ProjectCard"
+
+const ProjectsList = () => {
+  return (
+    <div>
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </div>
+  )
+}
+
+export default ProjectsList
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout"
 import * as React from "react"
 import ProjectCard from "../components/Cards/ProjectCard"
 import { useState } from "react"
-
+import ProjectsList from "./../components/Projects/ProjectsList"
 // markup
 const Projects = () => {
   const projects = [
@@ -103,15 +103,8 @@ const Projects = () => {
                   : "projects-content"
               }
             >
-              <h2>Content 1</h2>
-              <hr />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Obcaecati praesentium incidunt quia aspernatur quasi quidem
-                facilis quo nihil vel voluptatum?
-              </p>
+              <ProjectsList />
             </div>
-
             <div
               className={
                 toggleState === 2
@@ -119,12 +112,7 @@ const Projects = () => {
                   : "projects-content"
               }
             >
-              <h2>Content 2</h2>
-              <hr />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sapiente voluptatum qui adipisci.
-              </p>
+              <ProjectsList />
             </div>
 
             <div
@@ -134,16 +122,7 @@ const Projects = () => {
                   : "projects-content"
               }
             >
-              <h2>Content 3</h2>
-              <hr />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
-                sed nostrum rerum laudantium totam unde adipisci incidunt modi
-                alias! Accusamus in quia odit aspernatur provident et ad vel
-                distinctio recusandae totam quidem repudiandae omnis veritatis
-                nostrum laboriosam architecto optio rem, dignissimos voluptatum
-                beatae aperiam voluptatem atque. Beatae rerum dolores sunt.
-              </p>
+              <ProjectsList />
             </div>
             <div
               className={
@@ -152,16 +131,7 @@ const Projects = () => {
                   : "projects-content"
               }
             >
-              <h2>Content 4</h2>
-              <hr />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
-                sed nostrum rerum laudantium totam unde adipisci incidunt modi
-                alias! Accusamus in quia odit aspernatur provident et ad vel
-                distinctio recusandae totam quidem repudiandae omnis veritatis
-                nostrum laboriosam architecto optio rem, dignissimos voluptatum
-                beatae aperiam voluptatem atque. Beatae rerum dolores sunt.
-              </p>
+              <ProjectsList />
             </div>
           </div>
         </div>
