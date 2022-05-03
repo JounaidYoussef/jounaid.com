6932d182a09fd77cbb681ad2a7313b9acecd3c06
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-05-03 23:38:02 +0200

    add "Go To Top" Feature

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,7 @@
 import Footer from "../Footer"
 import Header from "../Header"
-import React from "react"
+import React, { useState } from "react"
+import "./index.scss"
 
 const styles = {
   color: "#232129",
@@ -13,12 +14,32 @@ const container = {
 }
 
 export default ({ iscontact, children, isFooterDisabled = false }) => {
+  const [showScroll, setShowScroll] = useState(false)
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 500) {
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 500) {
+      setShowScroll(false)
+    }
+  }
+  window.addEventListener("scroll", checkScrollTop)
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }
   return (
     <div style={container}>
       <div style={styles}>
         <Header iscontact={iscontact} />
         {children}
       </div>
+      <button
+        className="scrollTop"
+        onClick={scrollTop}
+        style={{ height: 40, display: showScroll ? "flex" : "none" }}
+      >
+        Top
+      </button>
       {isFooterDisabled ? null : <Footer />}
     </div>
   )
