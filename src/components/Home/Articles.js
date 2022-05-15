cdb078cbca5b53765b77f6981c86694f43830c2d
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-05-15 23:40:52 +0200

    Set Articles Component Boilerplate

--- src/components/Home/Articles.js
+++ src/components/Home/Articles.js
@@ -1,12 +1,23 @@
 import "./index.scss"
 import React from "react"
+import ProjectCard from "../Cards/ProjectCard"
+import DrawnArrow from "../../assets/drawnArrow.svg"
 
 export default () => {
   return (
-    <div className="articles-container">
-      <div>
-        <p>articles</p>
+    <div className="work-container icenter col">
+      <h2>Featured Work</h2>
+      <div className="row">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
       </div>
+      <a href="#" className="see-all">
+        <div>See the full blog</div>
+        <div style={{ transform: "rotate(90deg)", marginLeft: "4px" }}>
+          <DrawnArrow height="24px" width="24px" fill="#527693" />
+        </div>
+      </a>
     </div>
   )
 }
--- src/components/Home/Work.js
+++ src/components/Home/Work.js
@@ -14,7 +14,7 @@ export default () => {
         <ProjectCard />
       </div>
       <a href="#" className="see-all">
-        <div>See All Projects </div>
+        <div>See all projects </div>
         <div style={{ transform: "rotate(90deg)", marginLeft: "4px" }}>
           <DrawnArrow height="24px" width="24px" fill="#527693" />
         </div>
