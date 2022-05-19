25fb5081900f20c473fcde6f7c60ce79c7aba6b7
Youssef Jounaid <jounaid.ysf@gmail.com>
2022-05-19 17:26:51 +0200

    update skills UI

--- src/components/Home/Skills.js
+++ src/components/Home/Skills.js
@@ -1,5 +1,5 @@
-import D from "../../assets/d.svg"
-import M from "../../assets/m.svg"
+import W from "../../assets/w.svg"
+import Mm from "../../assets/mm.svg"
 import U from "../../assets/u.svg"
 import "./index.scss"
 import React from "react"
@@ -10,41 +10,29 @@ export default () => {
       <div className="skills-title">
         <h2>What I Do</h2>
       </div>
-      <div className="intro-container">
+      <div className="row-skills-container">
         <div className="skill-container">
           <div className="image-container">
-            <D width="140px" height="140px" />
+            <W width="72px" height="72px" />
           </div>
           <div>
-            <p id="skills-section-title">FullStack Web Development</p>
-            <p id="skills-section-text">
-              Turning up your ideas into usable MVPs, Developing websites using
-              MERN, MEAN or JAM stack
-            </p>
+            <p id="skills-section-title">Web Development</p>
           </div>
         </div>
         <div className="skill-container">
           <div className="image-container">
-            <M width="140px" height="140px" />
+            <Mm width="72px" height="72px" fill="#4c596a" />{" "}
           </div>
           <div>
             <p id="skills-section-title">Mobile Development</p>
-            <p id="skills-section-text">
-              Mainly with React Native, I work on creating CrossPlatform
-              (Android and iOS) Mobile Apps from scratch
-            </p>
           </div>
         </div>
         <div className="skill-container">
           <div className="image-container">
-            <U width="140px" height="140px" />
+            <U width="72px" height="72px" />
           </div>
           <div>
             <p id="skills-section-title">UI/UX Design</p>
-            <p id="skills-section-text">
-              Designing and optimizing user interfaces focusing on design,
-              usability, function, and even branding and marketing
-            </p>
           </div>
         </div>
       </div>
