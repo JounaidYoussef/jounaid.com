b2d5e603d03f07fc5a802f212b8107f32e0d3d74
Youssef Jounaid <jounaid.ysf@gmail.com>
2025-03-11 17:41:51 +0100

    integrate lottie and change content (Header,Introduction and Blog)

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
 import type { AstroIntegration } from 'astro';
+import lottie from 'astro-integration-lottie';
 
 import astrowind from './vendor/integration';
 
@@ -25,6 +26,7 @@ export default defineConfig({
   output: 'static',
 
   integrations: [
+    lottie(),
     tailwind({
       applyBaseStyles: false,
     }),
